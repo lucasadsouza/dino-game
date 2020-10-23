37514b4f9cbdb22ad56fb212bd84236847527ae0
Lucas Souza <50783437+varlasouza@users.noreply.github.com>
2020-10-23 15:22:44 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Dino Game
+
+
+Neste projeto foi recriado o jogo do dinossauro sem internet baseado na aula do professor Celso Henrique. Trata-se de um jogo simples, porém viciante.
+
+
+
+## Como jogar:
+
+* Aperte espaço para fazer o dinossauro pular;
+
+* Evite quantos cactos for possível.
+
+
+
+## As funções implementadas são basicamente:
+
+* Receber uma entrada via evento de teclado (a tecla espaço);
+* Pular (o dinossauro realiza um salto. Em meu projeto tem o adicional de uma formula simples de aceleração para dar dinámica ao pulo);
+* Criar um cacto (cria cactos em uma posição especifica com uma diferença de tempo aleátoria. Aqui temos um outro adicional em meu projeto, um espaço de tempo fixo para que os cactos não apareçam muito próximos uns dos outros);
+* Contator de cactos evitados (outro adicional ao meu projeto, considerei interessante adicionar um "score" para o jogador);
+* Score na tela de Game Over.
+
+
+
+var L. A. Souza	e-mail: lucasadsouza@gmail.com
+
+Celso Henrique	https://github.com/celso-henrique
---
+++ script.js
@@ -0,0 +1,107 @@
+//Dino-Game by var L. A. Souza based on Dino Game made by https://github.com/celso-henrique
+//e-mail: lucasadsouza@gmail.com
+
+
+const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+let isJumping = false;
+let position = 0;
+let gameOver = false;
+
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
+}
+
+
+function jump() {
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        let aceleration = 0;
+
+        if (position >= 200) {
+            clearInterval(upInterval);
+            aceleration = 0;
+
+            //-jump
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+
+                } else {
+                    position -= (20 + aceleration);
+                    dino.style.bottom = `${position}px`;
+
+                    if (position === 14.799999999999972) {
+                        aceleration = -5.200000000000028;
+                    }else{
+                        aceleration += 0.9;
+                    }
+                }
+            }, 20);
+
+        } else{
+            //+jump
+            position += (20 - aceleration);
+            dino.style.bottom = `${position}px`;
+            aceleration += 0.9;
+        }
+    }, 20);
+}
+
+
+function countScore() { //terminar
+    actualScore = Number(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = `${actualScore + 1}`;
+
+    return actualScore;
+}
+
+
+function createCactus() {
+    const cactus = document.createElement("div");
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+    while(randomTime < 400) {
+        randomTime = Math.random() * 6000;
+    }
+
+    console.log(randomTime);
+
+    cactus.classList.add("cactus");
+    cactus.style.left = "1000px";
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+            countScore();
+        
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = `<h1 class="game-over">Fim de Jogo</h1><p id="end-score">${countScore()}</p><p id="end-message">Recarregue a página para jogar novamente</p>`
+            gameOver = true;
+
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = `${cactusPosition}px`;
+        }
+    }, 20);
+
+    if (!gameOver) {
+        setTimeout(createCactus, randomTime);
+    }
+}
+
+
+createCactus();
+
+document.addEventListener("keyup", handleKeyUp);
